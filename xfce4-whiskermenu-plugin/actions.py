7bea98ca72d8a37af15144fda32afa515c8bfcca
pisiganesh <iyer.gb.linux@gmail.com>
2013-12-03 00:31:27 +0600

    updated build files

--- xfce4-whiskermenu-plugin/actions.py
+++ xfce4-whiskermenu-plugin/actions.py
@@ -6,17 +6,32 @@
 from pisi.actionsapi import cmaketools
 from pisi.actionsapi import get
 #from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 
 # if pisi can't find source directory, see /var/pisi/xfce4-whiskermenu-plugin/work/ and:
 # WorkDir="xfce4-whiskermenu-plugin-"+ get.srcVERSION() +"/sub_project_dir/"
 
+WorkDir = "."
+
 def setup():
+
+    shelltools.system("rpm2tar xfce4-whiskermenu-plugin-1.1.1-1.fc21.src.rpm")
+    shelltools.system("tar -xf xfce4-whiskermenu-plugin-1.1.1-1.fc21.src.tar")
+    shelltools.system("rm -f xfce4-whiskermenu-plugin.spec")
+    shelltools.system("bzip2 -d xfce4-whiskermenu-plugin-1.1.1-src.tar.bz2")
+    shelltools.system("tar -xf xfce4-whiskermenu-plugin-1.1.1-src.tar")
+    
+    shelltools.cd("xfce4-whiskermenu-plugin-1.1.1")
     cmaketools.configure("-DCMAKE_BUILD_TYPE=release", installPrefix="/usr")
 
 def build():
+    
+    shelltools.cd("xfce4-whiskermenu-plugin-1.1.1")
     cmaketools.make()
 
 def install():
+
+    shelltools.cd("xfce4-whiskermenu-plugin-1.1.1")
     cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
 
 # Take a look at the source folder for these file as documentation.
